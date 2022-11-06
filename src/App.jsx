67bdf6e7c08b4aba214978500cfd6b53e5195c68
marius-lionte <mariusmihai.learning@gmail.com>
2022-11-06 09:54:18 +0200

    Revert "remove subtitle"
    This reverts commit 37f4e465e3471deb36208bb241dd69e1a9238eb8.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h2>Dev Environment</h2>
       </header>
       <MainContent />
     </>
